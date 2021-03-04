a5e4e3c9f9890cac605b681d663909648add071b
Salih <Salih@DESKTOP-Q8NRVSB>
2021-03-05 02:26:13 +0300

    tekrar

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        InMemoryCarDal _ınMemoryCarDal;
+        public CarManager(InMemoryCarDal memoryCarDal)
+        {
+            _ınMemoryCarDal = memoryCarDal;
+        }
+        public void Add(Car car)
+        {
+            _ınMemoryCarDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _ınMemoryCarDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _ınMemoryCarDal.GetAll();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _ınMemoryCarDal.GetById(id);
+        }
+
+        public void Update(Car car)
+        {
+            _ınMemoryCarDal.Update(car);
+        }
+
+        
+    }
+}
--- ConsoleUi/Program.cs
+++ ConsoleUi/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
 
 namespace ConsoleUi
 {
@@ -6,8 +10,78 @@ namespace ConsoleUi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CarManager carManager = new CarManager(new InMemoryCarDal());
             
+            List<Car> cars = carManager.GetAll();
+
+            Car car1 = new Car
+            {
+                CarId = 7,
+                BrandId = 4,
+                ColorId = 2,
+                DailyPrice = 490,
+                ModelYear = 2014,
+                Description = "Mercedes"
+            };
+
+            Car car2 = new Car
+            {
+                CarId = 8,
+                BrandId = 2,
+                ColorId = 3,
+                DailyPrice = 805,
+                ModelYear = 2019,
+                Description = "Lexus"
+            };
+
+            //Kayıt Ekleme
+            carManager.Add(car1);
+            carManager.Add(car2);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine("Id : " + car.CarId);
+                Console.WriteLine("BrandId: " + car.BrandId);
+                Console.WriteLine("ColorId : " + car.ColorId);
+                Console.WriteLine("DailyPrice : " + car.DailyPrice);
+                Console.WriteLine("ModelYear : " + car.ModelYear);
+                Console.WriteLine("Description : " + car.Description);
+                Console.WriteLine("------------------------------------------");
+            }
+
+            //Kayıt Silme
+            //var d= carManager.GetById(4);
+            //carManager.Delete(d);
+
+            
+            carManager.Delete(car1);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine("Id : " + car.CarId);
+                Console.WriteLine("BrandId: " + car.BrandId);
+                Console.WriteLine("ColorId : " + car.ColorId);
+                Console.WriteLine("DailyPrice : " + car.DailyPrice);
+                Console.WriteLine("ModelYear : " + car.ModelYear);
+                Console.WriteLine("Description : " + car.Description);
+                Console.WriteLine("------------------------------------------");
+            }
+
+            //Referansta Tutarak Kayıt Güncelleme
+            car2.DailyPrice = 250;
+            carManager.Update(car2);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine("Id : " + car.CarId);
+                Console.WriteLine("BrandId: " + car.BrandId);
+                Console.WriteLine("ColorId : " + car.ColorId);
+                Console.WriteLine("DailyPrice : " + car.DailyPrice);
+                Console.WriteLine("ModelYear : " + car.ModelYear);
+                Console.WriteLine("Description : " + car.Description);
+                Console.WriteLine("------------------------------------------");
+            }
+
             Console.ReadKey();
         }
     }
